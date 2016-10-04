958132b011e2dd664d0b0ef9f957034b8464e257
Daniel Dreibrodt <daniel.dreibrodt@gmail.com>
2016-10-04 19:21:27 +0200

    Use NetCard variable in ifdown/ifup and minor changes
    Corrected log message
    Replaced tabs by spaces
    Indent with 3 spaces

--- NetReconnector.sh
+++ NetReconnector.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # by nickwang wxlcat@gmail.com
 
-NetCard=wlan0	 		# the netcard connected to the internet
-PingTarget=www.baidu.com	# the ping target, router ip or website, etc: 192.168.1.1, www.baidu.com
-LogFile=~/NetReconnector.log	# log file
-LogAll=false			# log msg no matter whether the net is connected, used to be check whether the script is running.
+NetCard=wlan0                # the netcard connected to the internet
+PingTarget=www.baidu.com     # the ping target, router ip or website, etc: 192.168.1.1, www.baidu.com
+LogFile=~/NetReconnector.log # log file
+LogAll=false                 # log msg no matter whether the net is connected, used to be check whether the script is running.
 
 test -e $LogFile || touch $LogFile
 
@@ -13,12 +13,12 @@ time=$(date "+%Y/%m/%d %H:%M:%S")
 ret=$(ping -c 2 -W 1 -I $NetCard $PingTarget | grep 'received' | cut -d ',' -f 2 | cut -d ' ' -f 2)
 
 if [ "$ret" == "0" ]; then
-    sudo ifdown wlan0 && sudo ifup wlan0
-    echo "Try Reconnect: $?, $time" | tee -a $LogFile
+   sudo ifdown $NetCard && sudo ifup $NetCard
+   echo "Trying to reconnect: $?, $time" | tee -a $LogFile
 else
-    if [ "$LogAll" == "true" ]; then
-        echo "Network is ok, ret = $ret, $time" | tee -a $LogFile
-    fi
+   if [ "$LogAll" == "true" ]; then
+      echo "Network is ok, ret = $ret, $time" | tee -a $LogFile
+   fi
 fi
 
 exit 0
